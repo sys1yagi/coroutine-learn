5c12f68c9a1c42edff90fb394e293c15810b083d
Toshihiro Yagi <sylc.yagi@gmail.com>
2018-03-13 21:07:53 +0900

    Create LICENSE

--- app/src/test/java/com/sys1yagi/coroutine_learn/CoroutineTest.kt
+++ app/src/test/java/com/sys1yagi/coroutine_learn/CoroutineTest.kt
@@ -4,6 +4,7 @@ import kotlinx.coroutines.experimental.delay
 import kotlinx.coroutines.experimental.runBlocking
 import org.junit.Test
 import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 
 class CoroutineTest {
     @Test
@@ -14,25 +15,47 @@ class CoroutineTest {
         println("end: ${System.currentTimeMillis() - start}")
     }
 
-
-    //    @Test
-//    fun handmadeDelay() {
-//
-//    }
-//
+    @Test
+    fun handmadeDelay() {
+        val dispatcher = SingleThreadDispatcher()
+        val coroutine = HandmadeDelay(dispatcher)
+        dispatcher.dispatch {
+            coroutine.resume()
+        }
+        dispatcher.await()
+    }
 
     interface Continuation {
         fun resume()
         fun resumeWithException(e: Exception)
     }
 
-    class Delay(val continuation: Continuation) {
-        fun delay(time: Long) {
-            val callThread = Thread.currentThread()
+    interface Dispatcher {
+        fun dispatch(runnable: () -> Unit)
+    }
+
+    class SingleThreadDispatcher : Dispatcher {
+        private val executor = Executors.newSingleThreadExecutor()
+
+        override fun dispatch(runnable: () -> Unit) {
+            executor.submit(runnable)
+        }
+
+        fun await(){
+            executor.awaitTermination(1, TimeUnit.SECONDS)
         }
     }
 
-    class HandmadeDelay : Continuation {
+    class Delay(val dispatcher: Dispatcher, val continuation: Continuation) {
+        fun delay(time: Long) {
+            dispatcher.dispatch {
+                Thread.sleep(time)
+                continuation.resume()
+            }
+        }
+    }
+
+    class HandmadeDelay(val dispatcher: Dispatcher) : Continuation {
         var state = 0
 
         // coroutine variable
@@ -44,10 +67,10 @@ class CoroutineTest {
                     start = System.currentTimeMillis()
                     println("start")
                     state++
-                    Delay(this).delay(100)
+                    Delay(dispatcher, this).delay(100)
                 }
                 1 -> {
-
+                    println("end: ${System.currentTimeMillis() - start}")
                 }
             }
         }
