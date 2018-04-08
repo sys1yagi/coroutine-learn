75cbf67399f0941b9e61f2e34d295a45057cde56
Toshihiro Yagi <sylc.yagi@gmail.com>
2018-04-08 19:10:25 +0900

    Update samples.

--- app/src/main/java/com/sys1yagi/coroutine_learn/MainActivity.kt
+++ app/src/main/java/com/sys1yagi/coroutine_learn/MainActivity.kt
@@ -2,11 +2,92 @@ package com.sys1yagi.coroutine_learn
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.experimental.Deferred
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.launch
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // kotlin coroutine
+//        launch(UI) {
+//            result.text = "loading..."
+//            result.text = async { loadData() }.await()
+//        }
+        CoroutineLaunchImpl(result, UIThreadDispatcher).resume()
+    }
+}
+
+fun loadData(): String {
+    Thread.sleep(1000L)
+    return "success!"
+
+}
+
+interface Continuation {
+    fun resume(value: Any? = null)
+}
+
+interface Dispatcher {
+    fun dispatch(runnable: () -> Unit)
+}
+
+object SingleThreadDispatcher : Dispatcher {
+    private val executor = Executors.newSingleThreadExecutor()
+
+    override fun dispatch(runnable: () -> Unit) {
+        executor.submit(runnable)
+    }
+}
+
+object UIThreadDispatcher : Dispatcher {
+    private val handler = Handler(Looper.getMainLooper())
+
+    override fun dispatch(runnable: () -> Unit) {
+        handler.post(runnable)
+    }
+}
+
+class CoroutineLaunchImpl(val result: TextView, private val dispatcher: Dispatcher) : Continuation {
+    var state = 0
+    var start: Long = 0
+
+    override fun resume(value: Any?) {
+        dispatcher.dispatch {
+            when (state) {
+                0 -> {
+                    result.text = "loading..."
+                    state++
+                    CoroutineAsyncImpl(SingleThreadDispatcher, this).resume()
+                }
+                1 -> {
+                    result.text = value as String
+                }
+            }
+        }
+    }
+}
+
+class CoroutineAsyncImpl(private val dispatcher: Dispatcher, val continuation: Continuation) : Continuation {
+    var state = 0
+
+    override fun resume(value: Any?) {
+        dispatcher.dispatch {
+            when (state) {
+                0 -> {
+                    val data = loadData()
+                    continuation.resume(data)
+                }
+            }
+        }
     }
 }
--- app/src/test/java/com/sys1yagi/coroutine_learn/CoroutineTest.kt
+++ app/src/test/java/com/sys1yagi/coroutine_learn/CoroutineTest.kt
@@ -1,6 +1,8 @@
 package com.sys1yagi.coroutine_learn
 
+import kotlinx.coroutines.experimental.CommonPool
 import kotlinx.coroutines.experimental.delay
+import kotlinx.coroutines.experimental.launch
 import kotlinx.coroutines.experimental.runBlocking
 import org.junit.Test
 import java.util.concurrent.Executors
@@ -8,75 +10,62 @@ import java.util.concurrent.TimeUnit
 
 class CoroutineTest {
     @Test
-    fun delayTest() = runBlocking<Unit> {
-        val start = System.currentTimeMillis()
-        println("start")
-        delay(100)
-        println("end: ${System.currentTimeMillis() - start}")
+    fun delayTest() {
+        runBlocking<Unit>(CommonPool) {
+            val start = System.currentTimeMillis()
+            println("start ${Thread.currentThread().id}")
+            delay(100)
+            println("end: ${System.currentTimeMillis() - start}")
+        }
     }
 
     @Test
-    fun handmadeDelay() {
-        val dispatcher = SingleThreadDispatcher()
-        val coroutine = HandmadeDelay(dispatcher)
+    fun handMadeDelayTest() {
+        CoroutineImpl(SingleThreadDispatcher).resume()
+        Thread.sleep(300)
+    }
+}
+
+interface Continuation {
+    fun resume()
+}
+
+interface Dispatcher {
+    fun dispatch(runnable: () -> Unit)
+}
+
+object SingleThreadDispatcher : Dispatcher {
+    private val executor = Executors.newSingleThreadExecutor()
+
+    override fun dispatch(runnable: () -> Unit) {
+        executor.submit(runnable)
+    }
+}
+
+class CoroutineImpl(private val dispatcher: Dispatcher) : Continuation {
+    var state = 0
+    var start: Long = 0
+
+    override fun resume() {
         dispatcher.dispatch {
-            coroutine.resume()
-        }
-        dispatcher.await()
-    }
-
-    interface Continuation {
-        fun resume()
-        fun resumeWithException(e: Exception)
-    }
-
-    interface Dispatcher {
-        fun dispatch(runnable: () -> Unit)
-    }
-
-    class SingleThreadDispatcher : Dispatcher {
-        private val executor = Executors.newSingleThreadExecutor()
-
-        override fun dispatch(runnable: () -> Unit) {
-            executor.submit(runnable)
-        }
-
-        fun await(){
-            executor.awaitTermination(1, TimeUnit.SECONDS)
-        }
-    }
-
-    class Delay(val dispatcher: Dispatcher, val continuation: Continuation) {
-        fun delay(time: Long) {
-            dispatcher.dispatch {
-                Thread.sleep(time)
-                continuation.resume()
-            }
-        }
-    }
-
-    class HandmadeDelay(val dispatcher: Dispatcher) : Continuation {
-        var state = 0
-
-        // coroutine variable
-        var start: Long = 0
-
-        override fun resume() {
             when (state) {
                 0 -> {
                     start = System.currentTimeMillis()
                     println("start")
                     state++
-                    Delay(dispatcher, this).delay(100)
+                    delay(100, dispatcher, this)
                 }
                 1 -> {
                     println("end: ${System.currentTimeMillis() - start}")
                 }
             }
         }
+    }
+}
 
-        override fun resumeWithException(e: Exception) {
-
-        }
+fun delay(milliSeconds: Long, dispatcher: Dispatcher, continuation: Continuation) {
+    dispatcher.dispatch {
+        Thread.sleep(milliSeconds)
+        continuation.resume()
     }
 }
